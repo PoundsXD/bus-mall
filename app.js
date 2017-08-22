b51e3d95b7a79ecec01f145a866a2adadd00f6c9
Brian Wheeler <monsteraono@gmail.com>
2017-08-22 14:28:48 -0700

    Got three images to randomly display every time I click one of the images.

--- app.js
+++ app.js
@@ -1,54 +1,67 @@
 'use strict';
 
 var imgArray = [];
-function NewImage(src, clickCount) {
+function NewImage(src) {
   this.src = src;
-  this.clickCount = clickCount;
+  this.clickCount = 0;
+  this.timesDisplayed = 0;
   imgArray.push(this);
 }
-var bag = new NewImage('img/bag.jpg', 0);
-var banana = new NewImage('img/banana.jpg', 0);
-var bathroom = new NewImage('img/bathroom.jpg', 0);
-var boots = new NewImage('img/boots.jpg', 0);
-var breakfast = new NewImage('img/breakfast.jpg', 0);
-var bubblegum = new NewImage('img/bubblegum.jpg', 0);
-var chair = new NewImage('img/chair.jpg', 0);
-var cthulu = new NewImage('img/cthulhu.jpg', 0);
-var dogDuck = new NewImage('img/dogDuck.jpg', 0);
-var dragon = new NewImage('img/dragon.jpg', 0);
-var pen = new NewImage('img/pen.jpg', 0);
-var petSweep = new NewImage('img/petSweep.jpg', 0);
-var scissors = new NewImage('img/scissors.jpg', 0);
-var shark = new NewImage('img/shark.jpg', 0);
-var sweep = new NewImage('img/sweep.png', 0);
-var tauntaun = new NewImage('img/tauntaun.jpg', 0);
-var unicorn = new NewImage('img/unicorn.jpg', 0);
-var usb = new NewImage('img/usb.gif', 0);
-var waterCan = new NewImage('img/waterCan.jpg', 0);
-var wineGlass = new NewImage('img/wineGlass.jpg', 0);
+var bag = new NewImage('img/bag.jpg');
+var banana = new NewImage('img/banana.jpg');
+var bathroom = new NewImage('img/bathroom.jpg');
+var boots = new NewImage('img/boots.jpg');
+var breakfast = new NewImage('img/breakfast.jpg');
+var bubblegum = new NewImage('img/bubblegum.jpg');
+var chair = new NewImage('img/chair.jpg');
+var cthulu = new NewImage('img/cthulhu.jpg');
+var dogDuck = new NewImage('img/dogDuck.jpg');
+var dragon = new NewImage('img/dragon.jpg');
+var pen = new NewImage('img/pen.jpg');
+var petSweep = new NewImage('img/petSweep.jpg');
+var scissors = new NewImage('img/scissors.jpg');
+var shark = new NewImage('img/shark.jpg');
+var sweep = new NewImage('img/sweep.png');
+var tauntaun = new NewImage('img/tauntaun.jpg');
+var unicorn = new NewImage('img/unicorn.jpg');
+var usb = new NewImage('img/usb.gif');
+var waterCan = new NewImage('img/waterCan.jpg');
+var wineGlass = new NewImage('img/wineGlass.jpg');
 
 /*for loop for choosing pictures.*/
 var totalClickCount = 0;
 var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 var exclusion = [];
-for(totalClickCount = 0; totalClickCount < 25; totalClickCount++) {
-  exclusion = [];
+var display = [];
+var genFix = 0;
+var divContentArray = ['divContent1', 'divContent2', 'divContent3'];
+function genPictures() {
+  display = [];
   document.getElementById('img1').innerHTML = '';
   for(var i = 0; i < 1; i++) {
     var num = Math.floor(Math.random() * (19 - 1) + 1);
     exclusion.push(num);
+    display.push(num);
     for(var h = 0; h < 2; h++) {
       while(exclusion.includes(num)) {
         var num = Math.floor(Math.random() * (19 - 1) + 1);
       }
       exclusion.push(num);
+      display.push(num);
     }
     for(var j = 0; j < 3; j++){
       var firstImg = document.getElementById('img1');
       var imgCreate = document.createElement('img');
-      imgCreate.src = imgArray[exclusion[j]].src;
-      imgCreate.className += 'divContent';
+      imgCreate.src = imgArray[display[j]].src;
+      imgCreate.className += divContentArray[j];
       firstImg.appendChild(imgCreate);
+      imgCreate.addEventListener('click', genPictures);
     }
+    genFix++;
+  } if(genFix > 1){
+    genFix = 0;
+    exclusion = [];
   }
+  totalClickCount++;
 };
+genPictures();
